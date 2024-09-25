4698c1dafc95d68b1e5dc82ac8dd45f6d249fe5a
kisunken <kkirmey@gmail.com>
2024-09-25 17:10:28 +0400

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './store/index.js';
+import store from './store/index.ts';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router.jsx';
+import { router } from './router.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <App /> */}
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
